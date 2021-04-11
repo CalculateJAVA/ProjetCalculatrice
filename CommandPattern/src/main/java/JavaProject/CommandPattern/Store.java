3cdbbe14f2289c58406d7eae007d0bbb97ef2327
UVSQ21801909 <ouiza.hellal1999@gmail.com>
2021-04-11 20:56:05 +0200

    classStore

---
+++ CommandPattern/src/main/java/JavaProject/CommandPattern/Store.java
@@ -0,0 +1,27 @@
+package JavaProject.CommandPattern;
+import java.util.Stack;
+
+public class Store implements Undoable {
+		/* # RECEIVER: Stack<Double> */
+		
+	private double nombre;
+	private Stack<Double> pile;
+		
+	public Store(Stack<Double> pile, double nombre) {
+		this.pile = pile;
+		this.nombre = nombre;		
+	}
+		
+	@Override
+	public void apply() {
+		this.pile.push(nombre);
+	}
+		
+	@Override
+	public void undo() {
+		this.pile.pop();
+	}
+
+	
+
+}
