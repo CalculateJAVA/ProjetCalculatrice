4ad24132ab3fce1d778d6993ee8f54c043dbf8ca
UVSQ21503979 <cylia-adouane@hotmail.fr>
2021-04-11 23:33:57 +0200

    Exception 4

---
+++ CommandPattern/src/main/java/JavaProject/CommandPattern/MaxExcept.java
@@ -0,0 +1,18 @@
+package JavaProject.CommandPattern;
+/**
+ * @Cylia ADOUANE @ Ouiza HELLAL
+ * Exception renvoyée si la valeur est trop grande
+ */
+
+public class MaxExcept extends ArithmeticException {
+	
+		/**
+		 * Constructeur de l'exception
+		 */
+		public MaxExcept() {
+			super("Impossible de gerer ce nombre ,il est trop grand");
+		}
+	}
+
+
+}
