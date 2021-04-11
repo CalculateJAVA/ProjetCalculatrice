d0291e13b610acc661ae1b01f89801026314bcb7
UVSQ21503979 <cylia-adouane@hotmail.fr>
2021-04-11 23:07:15 +0200

    MAJ Class SaisieRPN

--- CommandPattern/src/main/java/JavaProject/CommandPattern/SaisieRPN.java
+++ CommandPattern/src/main/java/JavaProject/CommandPattern/SaisieRPN.java
@@ -28,6 +28,46 @@ public class SaisieRPN {
 		public void fermerScanner() {
 			scan.close();
 		}
+		
+		
+		public void saisie() {
+			this.initScanner(new Scanner(System.in));
 
+			//String input = ""; 
+			//String s = "";
+
+			while(m.ouvert()) {
+				System.out.println("Pour effectuer vos calculs ,veuillez Saisir soi un entier ou une opération ou bien exit pour sortir,tapez entrer");
+				System.out.println(m.affichagePile());
+			}
+
+			// checkNombreOperations();
+
+			System.out.println("Fin du programme");
+			this.fermerScanner();    
+		}
+		
+		
+		
+		//private boolean isOp(String str) {
+		//	return this.renvoieOp(str) != null;
+		//}
+		
+		
+		
+		/**
+		 * Retourne une opération
+		 */
+		private Operation renvoieOp(String s) {
+			for (Operation op : Operation.values())
+				if (s.equals(op.getSymbol()))
+					return op;
+			return null;
+		}
+
+
+
+
+	
 		
 }
