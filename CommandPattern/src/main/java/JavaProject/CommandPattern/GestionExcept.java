1890c0d44acca26f9086af7b9a3abcd8de04ea4f
UVSQ21503979 <cylia-adouane@hotmail.fr>
2021-04-11 23:42:19 +0200

    GestionExcept

---
+++ CommandPattern/src/main/java/JavaProject/CommandPattern/GestionExcept.java
@@ -0,0 +1,26 @@
+package JavaProject.CommandPattern;
+
+/**
+ * @Cylia ADOUANE @ Ouiza HELLAL
+ */
+
+public class GestionExcept {
+
+		private static final double MIN_VALUE = 1000000;
+		private static final double MAX_VALUE = 1000000;
+		
+		public static void gestionMinMax(double nb) {
+			if(nb > MAX_VALUE || nb < -MIN_VALUE) {
+				throw new MaxExcept();
+			}
+		}
+		
+		public static void handleStackSize(int actualSize, int wantedSize) {
+			if(actualSize < wantedSize) {
+				throw new ArgPileExcept();
+			}
+		}
+
+}
+
+
--- CommandPattern/src/main/java/JavaProject/CommandPattern/MaxExcept.java
+++ CommandPattern/src/main/java/JavaProject/CommandPattern/MaxExcept.java
@@ -15,4 +15,4 @@ public class MaxExcept extends ArithmeticException {
 	}
 
 
-}
+
