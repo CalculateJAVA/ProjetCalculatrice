f9d3ab99b3824e8a63323ffb82b6071fbe722073
UVSQ21801909 <ouiza.hellal1999@gmail.com>
2021-04-11 22:27:09 +0200

    MAJ classMoteurRPN

--- CommandPattern/src/main/java/JavaProject/CommandPattern/MoteurRPN.java
+++ CommandPattern/src/main/java/JavaProject/CommandPattern/MoteurRPN.java
@@ -30,7 +30,35 @@ public class MoteurRPN extends Interpreteur{
 		
 		
 		
+	/**
+	* appliquer une opération sur les deux dernières opérandes de la pile
+	*/
+		
+	public void appliquerOp(Operation op) {
+		this.execStore(new OpCommand(pile, op));
+	}
 
+		
+	/**
+	* afficher les opérandes de la pile
+	*/
+		
+	public String affichagePile() {
+		String str = "[";
+		for (Double d : this.pile) {
+			str += (d + " ");
+		}	
+		return str.trim() + "]";
+	}
+
+		
+		
+	/** 
+	* retourner la taille de la pile 
+	*/
+	public int getSizePile(){
+		return this.pile.size();
+	}
 	
 	
 	
