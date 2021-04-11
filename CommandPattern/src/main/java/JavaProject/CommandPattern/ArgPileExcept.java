18b077969d6233277b0e3ab913bd287386af38a7
UVSQ21801909 <ouiza.hellal1999@gmail.com>
2021-04-11 23:19:05 +0200

    Exception1

---
+++ CommandPattern/src/main/java/JavaProject/CommandPattern/ArgPileExcept.java
@@ -0,0 +1,19 @@
+package JavaProject.CommandPattern;
+
+/**
+ * Exception pour la pile
+ * @ Cylia ADOUANE @ Ouiza HELLAL
+ */
+
+public class ArgPileExcept extends IllegalArgumentException {
+
+		/**
+		 * Constructeur
+		 */
+		public ArgPileExcept() {
+			super("Pile vide ou pas assez d'operandes");
+		}
+
+	
+
+}
